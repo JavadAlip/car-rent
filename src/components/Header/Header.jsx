62a6eaeade1b20bfc5d4dd09025908f57cf6d6ea
JavadAlip <122251720+JavadAlip@users.noreply.github.com>
2023-09-08 11:20:32 +0530

    latest

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import { Container,Row,Col } from 'reactstrap'
+import { Link } from 'react-router-dom'
+import "../../styles/header.css"
 
 const Header = () => {
   return <Header className="header">
     <div className='header_top'>
       <Container>
         <Row>
-          <col lg="6" md ="6"sm='6'>
+          <Col lg="6" md ="6"sm='6'>
             <div className="header_top__left">
               <span>Need help?</span>
-              <div className="header_top_help">
-                
+              <span className="header_top_help">
+              <i class="ri-phone-fill"></i> +91 6574839001
+              </span>
               </div>
-            </div>
-          </col>
+          </Col>
+          <Col lg="6" md ="6"sm='6'>
+          <div className="header_top__right">
+            <Link><i class="ri-arrow-right-circle-line">Login</i></Link>
+            <Link><i class="ri-user-line">Register</i></Link>
+          </div>
+          </Col>
+          
         </Row>
       </Container>
     </div>
